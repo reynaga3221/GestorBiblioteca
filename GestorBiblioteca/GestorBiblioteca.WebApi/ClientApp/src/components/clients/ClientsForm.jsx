f8ad39b95fbf0526411392067dd076f758377273
Matias Garone <matutegarone00@outlook.com>
2020-10-29 17:29:35 -0300

    hhhh

--- GestorBiblioteca/GestorBiblioteca.WebApi/ClientApp/src/components/clients/ClientsForm.jsx
+++ GestorBiblioteca/GestorBiblioteca.WebApi/ClientApp/src/components/clients/ClientsForm.jsx
@@ -25,9 +25,9 @@ const ClientsForm = ({ classes, Client, setClient, handleAddClient, handleCleanC
                 {/*   <TextField value={book.publishedDate ? book.publishedDate : ''} label="Fecha de Publicacion" variant="outlined" size="small" onChange={(e) => setBook({ ...book, publishedDate: e.target.value })} style={{ marginBottom: 20, marginRight: 20 }} />  */}
                 <TextField type="number" value={Client.dni ? Client.dni : ''} label="DNI" variant="outlined" size="small" onChange={(e) => setClient({ ...Client, dni: e.target.value })} style={{ marginRight: 20 }} />
                 <TextField type="number" value={Client.telefono ? Client.telefono : ''} label="Telefono" variant="outlined" size="small" onChange={(e) => setClient({ ...Client, telefono: e.target.value })} style={{ marginRight: 20 }} />
-                <TextField value={Client.mail ? Client.mail : ''} label="Mail" variant="outlined" size="small" onChange={(e) => setClient({ ...Client, mail: e.target.value })} style={{ marginTop: 20 }} />
+                <TextField value={Client.mail ? Client.mail : ''} label="Mail" variant="outlined" size="small" onChange={(e) => setClient({ ...Client, mail: e.target.value })} style={{ marginRight: 20 }} />
             </Grid>
-            <Grid container justify="flex-end" alignItems="flex-end" item xs={12}>
+                <Grid container justify="flex-end" alignItems="flex-end" item xs={12}>
                 <Button color="primary" onClick={validateForm}> Aceptar</Button>
                 <Button color="secondary" onClick={handleCleanClient} >Cancelar</Button>
             </Grid>
